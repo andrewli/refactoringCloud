57eb68ce0b2e37f6969f412919200054aff9eb05
liudongwei <liudongwei@cmhi.chinamobile.com>
2022-04-09 18:10:54 +0800

    mybatis cursor 导出数据至本地文件

--- retransform/src/main/java/com/example/demo/service/mybatis/MybatisService.java
+++ retransform/src/main/java/com/example/demo/service/mybatis/MybatisService.java
@@ -14,6 +14,7 @@
 
 package com.example.demo.service.mybatis;
 
+import com.alibaba.fastjson.JSONObject;
 import com.example.demo.dao.DeviceUserSyncMapper;
 import com.example.demo.model.DeviceUserSync;
 import lombok.extern.slf4j.Slf4j;
@@ -23,7 +24,12 @@ import org.apache.ibatis.session.SqlSessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
+import java.util.Iterator;
 
 /**
  * @author liudongwei
@@ -36,8 +42,9 @@ import java.io.IOException;
 public class MybatisService {
 
 /*
-    try括号内的资源会在try语句结束后自动释放,Cursor 自动关闭
-    使用try后面跟随 () 括号，用来管理释放资源,所有实现Closeable的类声明都可以写在try的括号中，最常见的是流操作、socket操作等,括号中可以写多行语句
+    try括号内的资源会在try语句结束后自动释放,Cursor 自动关闭：
+    使用try后面跟随 () 括号，用来管理释放资源,所有实现Closeable的类声明都可以写在try的括号中（建议只把相关流声明放在小括号内），最常见的是流操作、socket操作等,括号中可以写多行语句
+    无论try内是否抛出异常，try{} 大括号内执行结束后都会调用close方式关闭流：try{} 内容 -> close()方法 -> catch exception 处理 -> finally 处理
     开启一个 SqlSession （实际上也代表了一个数据库连接），并保证它最后能关闭
     使用 SqlSession 来获得 Mapper 对象,保证得到的 Cursor 对象是打开状态的
 */
@@ -45,19 +52,58 @@ public class MybatisService {
     @Autowired
     private SqlSessionFactory sqlSessionFactory;
 
+    public static final String NEW_LINE = System.getProperty("line.separator");
+
+    public static final String filePath = "D:\\apprun\\file\\test0409.txt";
+
     public void scan(int limit) {
+
         try (
                 SqlSession sqlSession = sqlSessionFactory.openSession();
-                Cursor<DeviceUserSync> cursor = sqlSession.getMapper(DeviceUserSyncMapper.class).scan(limit)
+                Cursor<DeviceUserSync> cursor = sqlSession.getMapper(DeviceUserSyncMapper.class).scan(limit);
+                FileOutputStream fileOutputStream = new FileOutputStream(filePath, true);
+                FileChannel channel = fileOutputStream.getChannel();
         ) {
-            cursor.forEach(e -> {
-                log.info("load ");
-
-            });
+            File file = new File(filePath);
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            ByteBuffer byteBuffer =ByteBuffer.allocate(1024);
+            Iterator<DeviceUserSync> iterator = cursor.iterator();
+            while (iterator.hasNext()) {
+                DeviceUserSync e = iterator.next();
+                log.info("load index:{}", cursor.getCurrentIndex());
+                String lineText = JSONObject.toJSONString(e) + NEW_LINE;
+                byteBuffer.put(lineText.getBytes());
+                byteBuffer.flip();
+                channel.write(byteBuffer);
+                byteBuffer.clear();
+            }
         } catch (IOException e) {
             log.info("cursor query error:{}", e);
         }
 
 
+      /*  try (
+                SqlSession sqlSession = sqlSessionFactory.openSession();
+                Cursor<DeviceUserSync> cursor = sqlSession.getMapper(DeviceUserSyncMapper.class).scan(limit);
+                FileOutputStream out = new FileOutputStream(filePath, true);
+        ) {
+            File file = new File(filePath);
+                if (!file.getParentFile().exists()) {
+                   file.getParentFile().mkdirs();
+            }
+            Iterator<DeviceUserSync> iterator = cursor.iterator();
+            while (iterator.hasNext()) {
+                DeviceUserSync e = iterator.next();
+                log.info("load index:{}", cursor.getCurrentIndex());
+                String lineText = JSONObject.toJSONString(e) + NEW_LINE;
+                out.write(lineText.getBytes());
+            }
+        } catch (IOException e) {
+            log.info("cursor query error:{}", e);
+        }
+*/
     }
 }
+
